c8657aba0fdab465497865da3ca92f6d1670c312
PablooEs <pablo_j_es@hotmail.com>
2021-03-09 01:34:32 -0300

    Finished setup

---
+++ client/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import "./style.css";
+
+ReactDOM.render(
+  <App title="Pablo" href="https://www.google.com" />,
+  document.getElementById("root")
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,10 +2,10 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
+  entry: path.resolve(__dirname, "src/index.tsx"),
   mode: "development",
   output: {
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
   },
   module: {
@@ -19,7 +19,11 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
     ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -27,4 +31,8 @@ module.exports = {
       template: path.resolve(__dirname, "src/index.html"),
     }),
   ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    hot: true,
+  },
 };
